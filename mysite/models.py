03d0f673f2289358cd24b72e76e3ac6aead7b47b
chei <11056016@ntub.edu.tw>
2023-10-04 15:51:21 +0800

    新增Post物件跟管理介面

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model): #物件繼承models.Model
+    title=models.CharField(max_length=200) #字元/長度
+    slug=models.CharField(max_length=200)
+    body=models.TextField()#長文字 不用長度
+    pub_date=models.DateField(auto_now_add=True)#日期(自動取得現在時間)
+    
+    class Meta: 
+        ordering=('-pub_date',)#ordering 排序(反向日期排序) 
+     
+    def __str__(self) -> str: #-- python內建 --
+        return   self.title
