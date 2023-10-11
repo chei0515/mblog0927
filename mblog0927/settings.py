ad10ded0ac29b414170b5fe59c2cb5b65f98a8a0
chei <11056016@ntub.edu.tw>
2023-10-11 16:23:26 +0800

    1011

--- mblog0927/settings.py
+++ mblog0927/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mblog0927.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
 TIME_ZONE = 'Asia/Taipei'
 
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from mysite.models import Post
+from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
+def homepage(request):
+    posts=Post.objects.all()
+    now=datetime.now()
+    return render(request,'index.html',locals())
+
+def showpost(request,slug):
+    post=Post.objects.get(slug=slug)
+    return render(request,'post.html',locals())
+'''
+def homepage(request):
+    posts=Post.objects.all() #把資料庫的東西傳上來
+    post_lists=list()
+    for counter,post in enumerate(posts): #enumerate回傳兩個值(index,data)
+        post_lists.append(f'No.{counter}-{post} <br>') #br 換行
+    return HttpResponse(post_lists)
+'''
